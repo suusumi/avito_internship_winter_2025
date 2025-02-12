803aedc4299e2fe7725cb9713dc29654f7e92a4a
Sergey Groshev <wer113wer@yandex.ru>
2025-02-12 21:40:18 +0300

    [feat](app): wrap content in theme provider

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,28 @@
 import '../App.css'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {Container} from "@mui/material";
+import {Container, ThemeProvider} from "@mui/material";
 import {AllItemsPage} from "../pages/allItems";
 import {DetailedItemPage} from "../pages/detailedItem";
 import {FormPage} from "../pages/form";
+import {Header} from "../widgets/header";
+import {theme} from "../shared/theme/theme.ts";
 
 function App() {
 
     return (
-        <BrowserRouter>
-            <Container maxWidth="lg">
-                <Routes>
-                    <Route path="/" element={<AllItemsPage/>}/>
-                    <Route path="/item/:id" element={<DetailedItemPage/>}/>
-                    <Route path="/form" element={<FormPage/>}/>
-                </Routes>
-            </Container>
-        </BrowserRouter>
+        <ThemeProvider theme={theme}>
+            <BrowserRouter>
+                <Header/>
+                <Container maxWidth="lg">
+                    <Routes>
+                        <Route path="/" element={<AllItemsPage/>}/>
+                        <Route path="/item/:id" element={<DetailedItemPage/>}/>
+                        <Route path="/form" element={<FormPage/>}/>
+                    </Routes>
+                </Container>
+            </BrowserRouter>
+        </ThemeProvider>
+
     )
 }
 
