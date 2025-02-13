3855fd5b7855df31e7c1b0cfb035b3157989e2c0
Sergey Groshev <wer113wer@yandex.ru>
2025-02-13 23:59:47 +0300

    [style](Header): update Header styles

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,29 +1,74 @@
 import React from "react";
-import {AppBar, Button, Container, Toolbar, Typography} from "@mui/material";
-import {Link as RouterLink} from 'react-router-dom';
+import {
+    AppBar,
+    Button,
+    Container,
+    Toolbar,
+    Typography,
+    Box,
+} from "@mui/material";
+import {Link as RouterLink} from "react-router-dom";
 import {Search} from "../../../features/search";
 
 export const Header: React.FC = () => {
     return (
         <AppBar position="static">
             <Container maxWidth="lg">
-                <Toolbar>
-                    <Typography
-                        variant="h6"
-                        component={RouterLink}
-                        to="/"
-                        sx={{flexGrow: 1, textDecoration: 'none', color: 'inherit'}}
+                <Toolbar
+                    sx={{
+                        flexDirection: {xs: "column", md: "row"},
+                        alignItems: "center",
+                        justifyContent: "space-between",
+                        py: {xs: 2, md: 1},
+                    }}
+                >
+                    <Box
+                        sx={{
+                            width: {xs: "100%", md: "auto"},
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: {xs: "center", md: "flex-start"},
+                            mb: {xs: 1, md: 0},
+                        }}
                     >
-                        Avito
-                    </Typography>
-                    <Search/>
-                    <Button color="inherit" component={RouterLink} to="/items">
-                        Объявления
-                    </Button>
-                    <Button variant="contained" color="primary" component={RouterLink} sx={{textTransform: 'none'}}
-                            to="/form">
-                        Разместить объявление
-                    </Button>
+                        <Typography
+                            variant="h6"
+                            component={RouterLink}
+                            to="/"
+                            sx={{
+                                textDecoration: "none",
+                                color: "inherit",
+                            }}
+                        >
+                            Avito
+                        </Typography>
+                    </Box>
+                    <Box
+                        sx={{
+                            width: {xs: "100%", md: "50%"},
+                            mb: {xs: 2, md: 0},
+                        }}
+                    >
+                        <Search/>
+                    </Box>
+                    <Box
+                        sx={{
+                            width: {xs: "100%", md: "auto"},
+                            display: "flex",
+                            justifyContent: {xs: "center", md: "flex-end"},
+                        }}
+                    >
+                        <Button
+                            variant="contained"
+                            component={RouterLink}
+                            to="/form"
+                            sx={{
+                                width: {xs: "100%", md: "auto"},
+                            }}
+                        >
+                            Разместить объявление
+                        </Button>
+                    </Box>
                 </Toolbar>
             </Container>
         </AppBar>
