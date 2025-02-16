cd6e1ba2f6cde31830d565c49e3104645e5cee53
Sergey Groshev <wer113wer@yandex.ru>
2025-02-16 14:50:35 +0300

    [feat](all items page): add filters, items list

--- src/pages/allItems/ui/AllItemsPage.tsx
+++ src/pages/allItems/ui/AllItemsPage.tsx
@@ -1,10 +1,17 @@
 import * as React from "react";
-import {Typography} from "@mui/material";
+import {Grid} from "@mui/material";
+import {ItemsList} from "../../../features/itemsList";
+import {Filters} from "../../../features/filters";
 
 export const AllItemsPage: React.FC = () => {
     return (
-        <>
-        <Typography>AllItemsPage</Typography>
-        </>
+        <Grid container spacing={2} sx={{marginTop: '10px'}}>
+            <Grid item xs={12} md={4} lg={4}>
+                <Filters/>
+            </Grid>
+            <Grid item xs={12} md={8} lg={8}>
+                <ItemsList/>
+            </Grid>
+        </Grid>
     );
 };
